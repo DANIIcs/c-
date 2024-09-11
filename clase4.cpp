1411cf77372622db2dca2c5b8b4ba8cdf48c6e89
Daniel <humberto.caballero@utec.edu.pe>
2024-09-10 21:37:10 -0500

    clase 4

---
+++ clase4.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <conio.h>//Libreria para que el ejectuable se cierre con una tecla 
+using namespace std;
+
+/*
+//sintaxis while
+int main(){
+    int i;
+
+    i=1;
+
+    while(i<=10){
+        cout<<i<<endl;
+        i+=1; // tambien se puede escribir como i++;
+    }
+    getch();//comando de conio para que espere a q yo ejectue una tecla apar cerrar el prohgrama
+    return 0;
+}
+
+
+//sintaxis do while
+int main(){
+    int i;
+
+    i=1;
+
+    do{
+        cout<<i<<endl;
+        i++;
+    }while (i<=10);
+
+    return 0;
+}
+
+//sintaxis de for
+int main(){
+    int i;
+
+    for( i=10; i>=1; i--){
+        cout<<i<<endl;
+    }
+
+    getch();
+    return 0;
+}
+
+//Ejercicio 1
+int main(){
+    int numero;
+    
+    do{
+        cout<<"Digite un numero: ";cin>>numero;
+    }while ((numero<1) || (numero>10));//si esque el numero es menor a 1 o igual a 10 el do while se seguira ejecutando
+
+    for(int i=1; i<=20; i++){
+        cout<<numero<<"*"<<i<<" = "<<numero*i<<endl;
+    }
+    getch();    
+    return 0;
+}
+*/
+
+//Ejercicio 2
+int main(){
+
+    return 0;
+}
