327bff7f58964ac59cda21f7dfebaefb51ab4388
Daniel <humbertodanielcaballeros@gmail.com>
2024-10-11 18:38:31 -0500

    clase14

---
+++ clase14.cpp
@@ -0,0 +1,4 @@
+//una cola es una estructur de datos, caracterizda por ser una secuencia de elemtnos en la la operacion de insercion se realiza por un extremo y ña extraccion por el otro
+//es decir el primer en llegar es el primero en salir y el ultimo en llegar sera el ultimo en salir asi como una cola del banco
+
+
