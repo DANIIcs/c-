1e44fcb00fbaa97dacdc303deb73f87cbde7763d
Daniel <humbertodanielcaballeros@gmail.com>
2024-10-11 12:54:00 -0500

    clase 13

--- clase13.cpp
+++ clase13.cpp
@@ -35,4 +35,124 @@ para insertar elemtnos en la pila, solo hay que seguir 4 pasos:
 4 asignar el nuevo nodo a pila
 
 */
+/*
 
+estructura principal
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Nodo{
+    int dato;
+    Nodo *siguiente;
+};
+
+void agregarPila(Nodo *&,int);
+void sacarPila(Nodo *&, int &);
+
+int main(){
+    Nodo *pila=NULL;
+    int dato;
+
+    cout<<"Digite un numero: ";cin>>dato;
+    agregarPila(pila,dato);
+
+    cout<<"\nDigite otro numero: ";cin>>dato;
+    agregarPila(pila,dato);
+
+    cout<<"\nSacando los elemntos de la pila: ";
+    while(pila != NULL){ //mientras no sea el final de la pila
+        sacarPila(pila, dato);
+        if(pila!=NULL){
+            cout<<dato<<" , ";
+        }
+        else{
+            cout<<dato<<" . ";
+        }
+    }
+
+    return 0;
+}
+
+void agregarPila(Nodo *&pila,int n){
+    Nodo *nuevo_nodo = new Nodo();  //crear el espacio en memoraria 
+    nuevo_nodo->dato =n; // cargar el valor dento del nodo(dato)
+    nuevo_nodo->siguiente = pila; //cargar el puntero pela dento del nodo(sigueinte)
+    pila =nuevo_nodo; // asignar el nuevo nodo a pila
+
+    cout<<"\n Elemeto "<<n<<" agregado \n";
+}
+
+void sacarPila(Nodo *&pila, int &n){
+    Nodo *aux = pila; //creamos un nuevo nodo aux que nos ayudara a elimnar el nodo sin problemas
+    n = aux ->dato; // el aux hacemos que apunte a la pila , es decir al primer valor
+    pila= aux->siguiente; //la pila hacemos que apunte al siguiente nodo para que el nodo se pueda eliinar sin ningun problema
+    delete aux; //eliminamos el nodo
+}
+*/
+
+//ejercicio 1: HAcer un programa para agregar numeros enteros a una pila 
+// hasta que el usuario lo decida, despues mostrar todos los numeros introducidos en la pila
+
+
+
+#include <iostream>
+#include <stdlib.h>
+using namespace std;
+
+struct Nodo{
+    int dato;
+    Nodo *siguiente;
+};
+
+void agregarPila(Nodo *&,int);
+void sacarPila(Nodo *&, int &);
+
+
+int main(){
+    Nodo *pila =NULL;
+    int dato;
+    char rpt;
+
+    do{
+        cout<<"Digite un numero: ";
+        cin>>dato;
+        agregarPila(pila, dato);
+
+        cout<<"Quieres agregar otro elemento a la pila(s/n): ";
+        cin>>rpt;
+    }while((rpt == 'S') || (rpt == 's'));
+
+    cout<<"\nSacando todos los elemtno de la pila: ";
+    while(pila != NULL){ //mientras no sea el final de la pila
+        sacarPila(pila, dato);
+        if(pila!=NULL){
+            cout<<dato<<" , ";
+        }
+        else{
+            cout<<dato<<" . ";
+        }
+    }
+
+
+    return 0;
+}
+
+
+
+void agregarPila(Nodo *&pila,int n){
+    Nodo *nuevo_nodo = new Nodo();  //crear el espacio en memoraria 
+    nuevo_nodo->dato =n; // cargar el valor dento del nodo(dato)
+    nuevo_nodo->siguiente = pila; //cargar el puntero pela dento del nodo(sigueinte)
+    pila =nuevo_nodo; // asignar el nuevo nodo a pila
+
+    cout<<"\n Elemeto "<<n<<" agregado \n";
+}
+
+void sacarPila(Nodo *&pila, int &n){
+    Nodo *aux = pila; //creamos un nuevo nodo aux que nos ayudara a elimnar el nodo sin problemas
+    n = aux ->dato; // el aux hacemos que apunte a la pila , es decir al primer valor
+    pila= aux->siguiente; //la pila hacemos que apunte al siguiente nodo para que el nodo se pueda eliinar sin ningun problema
+    delete aux; //eliminamos el nodo
+}
