680302c3f6a8f676f3ca92ef39e800cf2127f7ca
Daniel <humbertodanielcaballeros@gmail.com>
2024-11-08 20:43:59 -0500

    a

---
+++ clase7.cpp
@@ -0,0 +1,101 @@
+//Cadenas de carcateres
+
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+
+using namespace std;
+/*
+int main(){
+    char palabra[] = "Alejandro";
+    char palabra2[] = {'A','l','e','j','a','n','d','r','o'};
+    char nombre[20];
+
+    cout<<"Digite su nombre: ";
+    cin.getline(nombre,20,'\n');
+    
+    cout<<nombre<<endl;
+
+    getch();
+    return 0;
+}
+
+int main(){
+    char palabra[] = "hola";
+    int longitud=0;
+
+    longitud = strlen(palabra);//sirve para saber el numeor de elemtos de una cadena
+
+    cout<<"Numero de elementos de la cadena es: "<<longitud<<endl;
+
+    getch;
+    return 0;
+}
+
+//saber si una cadena supero a 10 carcateres 
+int main(){
+    char nombre[50];
+
+    cout <<"Digita su nombre: ";;
+    cin.getline(nombre,50,'\n');
+
+    if(strlen(nombre)>10){
+        cout<<nombre<<endl;
+    }
+    else{
+        cout<<"El nombre no supero los 10 carcateres";
+    }
+}
+
+//la funcion strcpy copia el contenido de una cadena a otra
+int main(){
+    char nombre[]="Alejandro";
+    char nombre2[20];
+
+    strcpy(nombre2,nombre);//primero la cadena d caracteres vacio y luego a que quermeos copiar
+
+    cout<<nombre2<<endl;
+    getch();
+    return 0;
+}
+
+//ejercicio
+int main(){
+    char frase[50];
+    char frase2[50];
+
+    cout<<"Dame una cadena d carcaterres: ";
+    cin.getline(frase,50,'\n');
+    strcpy(frase2,frase);
+
+    cout<<frase2<<endl;
+
+    getch();
+    return 0;
+}
+//compara cadenas funcion strcmp()
+int main() {
+    char palabra[] = "hola";
+    char palabra2[] = "hola";
+
+    if (strcmp(palabra, palabra2) == 0) {
+        cout << "Ambas palabras son iguales";
+    }
+
+    getch();
+    return 0;
+}
+
+*/
+
+//ejercicio 
+int main(){
+
+    char frase[40];
+
+
+    getch;
+    return 0;
+}
+
+
